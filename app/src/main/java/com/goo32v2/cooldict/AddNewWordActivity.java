bb257911ee90f6da105f2a8214f3537febe32b37
goo32v2 <goo32v2@gmail.com>
2016-04-12 00:37:34 +0300

    init commit

---
+++ app/src/main/java/com/goo32v2/cooldict/AddNewWordActivity.java
@@ -0,0 +1,19 @@
+package com.goo32v2.cooldict;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_word);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
